10571c81fe293760cf8a7bf94c8ac0da43c597c1
Jennifer <jennifer.zaminer@gmail.com>
2021-11-02 11:51:13 +0100

    tests

---
+++ backend/src/test/java/de/dhbw/planit/usecase1.java
@@ -0,0 +1,56 @@
+package de.dhbw.planit;
+import io.cucumber.java8.En;
+public class usecase1 implements En {
+	public usecase1(){
+
+		@Given("I have {String} network connection" , (String networks) -> {
+			
+			return true;
+		});
+		
+		@When(/^I start the application$/, () => {
+			return true;
+		});
+		
+		@Then(/^I can not load the page$/, () => {
+			return true;
+		});
+		
+		@Then(/^I get an error$/, () => {
+			return true;
+		});
+		
+		@Given(/^I have saved appointments in local storage$/, () => {
+			return true;
+		});
+		
+		@Then(/^I have a network connection$/, () => {
+			return true;
+		});
+		
+		@When(/^I start the application$/, () => {
+			return true;
+		});
+		
+		Then(/^I see should see all saved appointments in the application$/, () => {
+			return true;
+		});
+		
+		@Given(/^I have no saved appointments in local storage$/, () => {
+			return true;
+		});
+		
+		@Then(/^I have a network connection$/, () => {
+			return true;
+		});
+		
+		@When(/^I start the application$/, () => {
+			return true;
+		});
+		
+		@Then(/^I see the application with an empty schedule$/, () => {
+			return true;
+		});
+		
+	}
+}
