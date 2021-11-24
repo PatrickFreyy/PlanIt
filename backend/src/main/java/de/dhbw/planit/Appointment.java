0935c38a0522e1cfa00538ae45a51a0d38680ee4
PatrickFreyy <patrick.langenalb@gmail.com>
2021-11-24 22:25:21 +0100

    classes initialized, not filled

---
+++ backend/src/main/java/de/dhbw/planit/Appointment.java
@@ -0,0 +1,13 @@
+package de.dhbw.planit;
+
+import java.util.Date;
+
+public class Appointment {
+      int app_ID;
+      String description;
+      Category category;
+      int priority;
+      Color color;
+      Date startTime;
+      Date endTime;
+}
---
+++ backend/src/main/java/de/dhbw/planit/Schedule.java
@@ -0,0 +1,5 @@
+package de.dhbw.planit;
+
+public class Schedule {
+      
+}
