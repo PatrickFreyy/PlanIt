9c56b3795fcc2084546b33294338a6abd8e2b295
Jennifer <jennifer.zaminer@gmail.com>
2021-12-11 18:28:21 +0100

    add schedule, with appointment paint

---
+++ src/main/java/de/dhbw/planit/frontend/Paint.java
@@ -0,0 +1,42 @@
+package Scheduler;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Date;
+
+import javax.swing.JComponent;
+
+public class Paint extends JComponent {
+	String description;
+	// Category category;
+	int priority; // data not final
+	Color color;
+	Date startDate = new Date();
+	Date endDate = new Date();
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		int time = getTimeInMinutes();
+		int x = 0;
+
+//		g.fillRect(0, 10, 30, 30);
+		g.setColor(Color.BLACK);
+		g.fillRect(x, getStart(), WeekOverview.uhrzeitenPanel.getWidth(), time);
+	}
+
+	private int getStart() {
+		return ((startDate.getHours() * 60) + startDate.getMinutes()) / (24 * 60);
+	}
+
+	private int getTimeInMinutes() {
+		this.endDate.setHours(11);
+		this.endDate.setMinutes(15);
+		this.startDate.setHours(10);
+		this.startDate.setMinutes(15);
+		int diffhrs = endDate.getHours() - startDate.getHours();
+		int diffmin = endDate.getMinutes() - startDate.getMinutes();
+		int length = diffhrs * 60 + diffmin;
+		int percentage = length / (24 * 60);
+		return percentage;
+	}
+}
